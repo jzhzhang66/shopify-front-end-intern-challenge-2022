7b74e615629d477e20b02673a1f3923d326ed71c
Judy Zhang <judyzhang@Judys-MacBook-Pro.local>
2022-01-07 17:52:40 -0500

    added copy url

--- components/post.jsx
+++ components/post.jsx
@@ -3,6 +3,19 @@ import { Icon } from '@iconify/react';
 
 export default function Post({ postInfo }) {
     const [liked, setLiked] = useState(false);
+    const [isClicked, setIsClicked] = useState(false);
+    const buttonText = isClicked ? "Copied to clipboard!" : "Copy image link";
+    const onClick = async e => {
+        setIsClicked(true);
+        setTimeout(() => setIsClicked(false), 3 * 1000);
+        e.preventDefault();
+        const el = document.createElement('input');
+        el.value = postInfo.url;
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+    }
 
     return <div className="post-container">
         <img src={postInfo.url} />
@@ -14,7 +27,10 @@ export default function Post({ postInfo }) {
                 <a>{postInfo.date}</a>
                 <br />
             </div>
-            <Icon className="heart" onClick={() => setLiked(prevLiked => !prevLiked)} icon={liked ? "ant-design:heart-filled" : "ant-design:heart-outlined"}  color={liked ? "pink" : "white"}  width="50" height="50" />
+            <Icon className="heart" onClick={() => setLiked(prevLiked => !prevLiked)} icon={liked ? "ant-design:heart-filled" : "ant-design:heart-outlined"} color={liked ? "pink" : "white"} width="50" height="50" />
         </div>
+        <button className="post-button" onClick={onClick} >
+            {buttonText}
+        </button>
     </div>
 }
